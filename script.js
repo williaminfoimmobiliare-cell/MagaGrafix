7732222f3bcd3894692c1720d0e2b59084b8d1ed
williaminfoimmobiliare-cell <william.info.immobiliare@gmail.com>
2025-11-06 18:01:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 /* ---- CONFIG ---- */
 const LS_KEY = 'magagrafix_app_v4';
 const LOW_STOCK_THRESHOLD = 4;
-const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwphH5Jn6vaoG63H-_Hzl4mFDGjg-OowI5gIjTu6OSA7ILV9tP27MSIT6zUFxYsGNb0/exec';
+const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwhVolw1DVIrkTCWdGElPUnH__tLvKANGAn4pmazFQGaQmmQrvj0GdPAvQh5NxShvsD/exec';
 
 /* ---- DATA ---- */
 let store = {
@@ -435,3 +435,4 @@ async function exportPeriodPDF() {
   pdf.addImage(img, 'PNG', 40, 70, 500, 300);
   pdf.save(`magagrafix_periodo_${from}_${to}.pdf`);
 }
+
